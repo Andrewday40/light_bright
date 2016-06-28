e28a7bbcad93f84b94a5f24ddbef9c503932e95a
Andrew <andrewday40@gmail.com>
2016-06-27 20:55:02 -0400

    added multiple cells through jquiry

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,19 @@
 
 $(document).ready(function(){
-  console.log('off to the races');
+  var container = $('.container');
+  var numOFRows = 10;
+  var numOFCols = 10;
+
+  initGrid();
+  function initGrid(){
+    for(var i = 0; i< numOFRows; i += 1){
+      var row = $('<div></div>');
+      for(var j = 0; j < numOFCols; j += 1){
+        var cell = $('<div></div>');
+        cell.addClass('cell border');
+        row.append(cell);
+      }
+      container.append(row);
+    }
+  }
 });
